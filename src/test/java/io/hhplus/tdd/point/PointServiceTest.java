addb4e0306890afcaea33c65c95932edf72fddba
hyeonwoody <hyeonwoody@gmail.com>
2024-06-18 02:11:06 +0900

    ✅TEST : 포인트 충전 테스트 코드 작성

--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -15,16 +15,16 @@ public class PointServiceTest {
     @BeforeEach
     void setUp(){
         UserPointTable userPointTable = new UserPointTable();
-        pointRepositoryImpl = new PointRepositoryImpl();
+        pointRepositoryImpl = new PointRepositoryImpl(userPointTable);
         pointServiceSpy = new PointServiceSpy(pointRepositoryImpl);
     }
 
     @Test
-    @DisplayName("포인트를 충전한다:Spy")
+    @DisplayName("포인트를 충전한다 : Spy 활용")
     void charge()  {
         //given : 포인트 충전 대상과 금액 설정
         long id = 1L;
-        long amount = 1000L;
+        long amount = 1234L;
 
         //when : 충전 전과 후의 금액 차를 모니터링 하기 위해 스파이 객체의 충전 메서드 호출
         pointServiceSpy.chargeAmount(id, amount);
