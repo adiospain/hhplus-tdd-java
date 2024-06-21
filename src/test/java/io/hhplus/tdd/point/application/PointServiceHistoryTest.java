f6556361c6b71c7bd0992fc92a36ff68952c1ee8
hyeonwoody <hyeonwoody@gmail.com>
2024-06-21 20:49:28 +0900

    ✅TEST : PointService 포인트 내역이 있는 사용자 내역 리스트 리턴

--- src/test/java/io/hhplus/tdd/point/application/PointServiceHistoryTest.java
+++ src/test/java/io/hhplus/tdd/point/application/PointServiceHistoryTest.java
@@ -1,6 +1,8 @@
 package io.hhplus.tdd.point.application;
 
+import io.hhplus.tdd.point.domain.TransactionType;
 import io.hhplus.tdd.point.dto.PointHistory;
+import io.hhplus.tdd.point.exception.PointException;
 import io.hhplus.tdd.point.infrastructure.PointRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -10,6 +12,7 @@ import org.mockito.Mockito;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -26,18 +29,64 @@ public class PointServiceHistoryTest {
     }
 
     @Test
-    @DisplayName("포인트 내역이 없는 사용자의 내역을 조회 한다")
-    void history() throws Exception {
+    @DisplayName("포인트 내역이 있는 사용자의 경우, 내역 리스트를 리턴")
+    void retrievePointHistory() throws Exception {
         //given
         long userId = 3L;
+        long amount = 1000L;
+
         long time = System.currentTimeMillis();
-        when(pointRepositoryMock.findHistoryById(userId)).thenReturn(List.of());
+        List<PointHistory> fakeHistories = List.of(
+                new PointHistory(1L,3L, 2000L, TransactionType.CHARGE, System.currentTimeMillis()),
+                new PointHistory(2L, 3L, 500L, TransactionType.USE, System.currentTimeMillis())
+
+        );
+
+        when(pointRepositoryMock.findHistoryByUserId(userId)).thenReturn(fakeHistories);
 
         //when
         List<PointHistory> histories = pointServiceMock.history(userId);
-        verify(pointRepositoryMock).findHistoryById(userId);
+        verify(pointRepositoryMock).findHistoryByUserId(userId);
+        //then
+        assertEquals(histories, fakeHistories);
+        assertEquals(histories.size(), fakeHistories.size());
+    }
+
+    @Test
+    @DisplayName("포인트 내역이 없는 사용자의 경우, 비어있는 내역 리스트를 리턴")
+    void retrievePointHistoryButEmpty() throws Exception {
+        //given
+        long userId = 3L;
+        long time = System.currentTimeMillis();
+        when(pointRepositoryMock.findHistoryByUserId(userId)).thenReturn(List.of());
+
+        //when
+        List<PointHistory> histories = pointServiceMock.history(userId);
+        verify(pointRepositoryMock).findHistoryByUserId(userId);
         //then
         assertEquals(List.of(), histories);
         assertEquals(histories.size(), 0);
+    }
+
+    @Test
+    @DisplayName("사용자 id가 음수인 경우, 실패")
+    void historyNegativeID_Fail(){
+        //given : 포인트 소지량
+        long id = -2L;
+
+        Exception pointException = assertThrows(PointException.class, ()->{
+            pointServiceMock.history(id);
+        });
+    }
+
+    @Test
+    @DisplayName("사용자 id가 0인 경우, 실패")
+    void historyZeroID_Fail(){
+        //given : 포인트 소지량
+        long id = 0L;
+
+        Exception pointException = assertThrows(PointException.class, ()->{
+            pointServiceMock.history(id);
+        });
     }
 }
